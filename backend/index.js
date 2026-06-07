e524c134be5607898d6404e9b78a7b08bd49db5a
Pruthviraj Sankpal <pruthvirajsankpal2003@gmail.com>
2026-06-07 20:35:42 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://quickbite-43lo.onrender.com],
     credentials:true,
     methods:['POST','GET']
 }) 
@@ -34,7 +34,7 @@ const io=new Server(server,{
 app.set('io',io)
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://quickbite-43lo.onrender.com'],
     credentials:true
 }))
 
